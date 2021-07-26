ea0fc7b45841de8300d8aee923b03b11d55919a5
Tiago Angelo <kurtis.angelo@gmail.com>
2021-07-26 09:56:48 -0300

    Extract download function

--- src/run.js
+++ src/run.js
@@ -1,25 +1,14 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const {platform, arch} = require("process");
+const {promisify} = require('util');
+
+const subprocesses = require('child_process');
+const exec = promisify(subprocesses.exec)
+
+const download = require('./download');
 
 async function run() {
-    const token = core.getInput(`github-token`, {required: true});
-    const {rest: {repos}} = github.getOctokit(token)
-
-    const owner = "ZupIT"
-    const repo = "horusec"
-
-    const {data: {name, assets, created_at}} = await repos.getLatestRelease({owner, repo});
-    core.info(`Using ${name} released at ${created_at}`)
-
-    const asset = assets.find(({name}) => name.includes(`${platform}_${arch}`))
-    if (!asset) {
-        throw new Error(`No binary for ${platform}_${arch}`)
-    }
-    core.info(`Found binary '${asset.name}' for current platform`)
-
-    delete asset.uploader
-    return asset
+    const executable = await download()
+    await exec(`${executable} start -p="./" --ignore="**/.vscode/**, **/*.env, **/.mypy_cache/**, **/tests/**"`)
 }
 
 run()
